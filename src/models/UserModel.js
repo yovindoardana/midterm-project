11aa309483ba13340af0713dd0c0203c75635e50
Yovindo Ardana <yovindoardanaa@gmail.com>
2023-07-23 21:26:09 +0700

    Create UserModel.js

---
+++ src/models/UserModel.js
@@ -0,0 +1,26 @@
+import {Schema, model} from 'mongoose';
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  fullname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profilePicture: {
+    type: String,
+    required: false
+  }
+});
+
+export default model('User', UserSchema);
