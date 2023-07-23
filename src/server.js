9c315dd90af6b5c62bc4b5e8082af34543b056cc
Yovindo Ardana <yovindoardanaa@gmail.com>
2023-07-24 00:56:43 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
 import connectToDatabase from './config/connection.js';
+import UserRoutes from './routes/UserRoutes.js';
+import VideoRoutes from './routes/VideoRoutes.js';
+import ProductRoutes from './routes/ProductRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-import UserRoutes from './routes/UserRoutes.js';
-import VideoRoutes from './routes/VideoRoutes.js';
 
 connectToDatabase();
 
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/users', UserRoutes);
 app.use('/videos', VideoRoutes);
+app.use('/products', ProductRoutes);
 
 app.get('/', (req, res) => {
   res.status(200).send('Server is Ready');
