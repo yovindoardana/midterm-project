f321bf84354a3e7aec6c247d037a523435436fbb
Yovindo Ardana <yovindoardanaa@gmail.com>
2023-07-23 22:03:23 +0700

    Complete UserController
    Get User By Id, Add User, Update User, Delete User

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -9,4 +9,43 @@ const getUsers = async (req, res) => {
   }
 };
 
-export default {getUsers};
+const getUserbyId = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({message: err.message});
+  }
+};
+
+const addUser = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({message: err.message});
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(404).json({message: err.message});
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({message: 'User deleted'});
+  } catch (err) {
+    res.status(404).json({message: err.message});
+  }
+};
+
+export default {getUsers, getUserbyId, addUser, updateUser, deleteUser};
